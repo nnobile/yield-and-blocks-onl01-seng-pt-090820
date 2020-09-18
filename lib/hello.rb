345e9160678941b9139f44a966e198fa82902d2e
Nick Nobile <nickcnobile@gmail.com>
2020-09-18 02:50:44 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,20 @@
-def hello_t
+def hello_t(array)
+  i = 0
 
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
 end
 
-# call your method here!
+ruby
+["Tim", "Tom", "Jim"]
+
+ruby
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
+
 
